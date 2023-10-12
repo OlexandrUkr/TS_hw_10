bdb4482e3c08ff7dc5c41ab97577a0c23f889f87
Olexandr <olexandrcv@gmail.com>
2023-10-12 18:55:40 +0300

    add 2

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 type ReturnTypeIfFunction<T extends (...args: any[]) => any> = T extends (...args: any[]) => infer U ? U : never;
 
 type ReturnTupleTypes<T> = T extends (param: infer U) => infer V ? [V, U] : never;
+
+type ReturnTypeIfFunction2<T> = T extends (...args: any[]) => infer U ? U : never;
+
+type ReturnTupleTypes2<T extends (param: any) => any> = T extends (param: infer U) => infer V ? [V, U] : never;
